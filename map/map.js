96477ea748d0b239284fe22965d9efe80ac4ae21
bwapes <benwaples@gmail.com>
2020-07-23 17:56:07 -0700

    if user has a completed quest, that task is not clickable

--- map/map.js
+++ map/map.js
@@ -1,9 +1,20 @@
+import { getUser } from '../userUtils.js';
 import allQuests from '../quest-data.js';
 
 const questList = document.querySelector('#quest-list');
 
 for (let i = 0; i < allQuests.length; i++) {
+    const user = getUser();
+
     const quest = allQuests[i];
+    
+    if (user.completed[quest.id]) {
+        const finishedQuest = document.createElement('span');
+        finishedQuest.textContent = quest.title;
+        finishedQuest.href = '/quest/?id=' + quest.id;
+
+        questList.append(finishedQuest);
+    }
     
     const questLink = document.createElement('a');
     questLink.textContent = quest.title;
