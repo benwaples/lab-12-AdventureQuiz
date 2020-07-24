c3bc8caa21cd81de8f1fa7f71435d7d827e18985
bwapes <benwaples@gmail.com>
2020-07-24 09:41:09 -0700

    logic if user completes the quests

--- map/map.js
+++ map/map.js
@@ -4,28 +4,40 @@ import allQuests from '../quest-data.js';
 const questList = document.querySelector('#quest-list');
 const user = getUser();
 
-if (user.hp <= 0) {
-    window.location = '../results/index.html';
+
+let conqueredQuests = 0;
+
+for (let i = 0; i < allQuests.length; i ++) {
+    const quest = allQuests[i];
+    if (user.completed[quest.id]) {
+        conqueredQuests++;
+    }
 }
 
-for (let i = 0; i < allQuests.length; i++) {
-
-    const quest = allQuests[i];
+if (user.hp <= 0) {
+    window.location = '../results/index.html';
+} else if (conqueredQuests === allQuests.length) {
+    window.location = '../results/index.html';
+} else {
+    for (let i = 0; i < allQuests.length; i++) {
     
-    if (user.completed[quest.id]) {
-        const finishedQuest = document.createElement('span');
-        finishedQuest.textContent = quest.title;
-        finishedQuest.href = '/quest/?id=' + quest.id;
-
-        questList.append(finishedQuest);
-    } else {
-        const questLink = document.createElement('a');
-        questLink.textContent = quest.title;
-        questLink.href = '/quest/?id=' + quest.id;
+        const quest = allQuests[i];
+        
+        if (user.completed[quest.id]) {
+            const finishedQuest = document.createElement('span');
+            finishedQuest.textContent = quest.title;
+            finishedQuest.href = '/quest/?id=' + quest.id;
     
-        questList.append(questLink);
-
+            questList.append(finishedQuest);
+        } else {
+            const questLink = document.createElement('a');
+            questLink.textContent = quest.title;
+            questLink.href = '/quest/?id=' + quest.id;
+        
+            questList.append(questLink);
+    
+        }
+        
     }
-    
 }
 
